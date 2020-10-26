77e9aceb55e3bb372e6f27389f7150856a294f78
simonojok19 <simonojok19@gmail.com>
2020-10-27 00:46:29 +0300

    extracted appbar to method

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,12 +5,16 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        leading: IconButton(
-          icon: SvgPicture.asset("assets/icons/menu.svg"),
-          onPressed: () {},
-        ),
+      appBar: buildAppBar(),
+    );
+  }
+
+  AppBar buildAppBar() {
+    return AppBar(
+      elevation: 0,
+      leading: IconButton(
+        icon: SvgPicture.asset("assets/icons/menu.svg"),
+        onPressed: () {},
       ),
     );
   }
