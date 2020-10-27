d3e6730ac971b1dba27946f31441060ed2e41e5d
simonojok19 <simonojok19@gmail.com>
2020-10-27 22:51:35 +0300

    refactored most codes

--- lib/screens/components/body.dart
+++ lib/screens/components/body.dart
@@ -6,6 +6,8 @@ import 'package:plant/constants.dart';
 import 'package:plant/screens/components/recommended_plants_widget.dart';
 import 'package:plant/screens/components/title_with_more_button.dart';
 
+import 'home_with_search_contianer.dart';
+
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,85 +21,5 @@ class Body extends StatelessWidget {
         ],
       ),
     );
-  }
-  Container buildHomeWithSearchContainer(Size size, BuildContext context) {
-    return Container(
-        margin: EdgeInsets.only(
-          bottom: kDefaultPadding * 0.5
-        ),
-        height: size.height * 0.2,
-        child: Stack(
-          children: [
-            Container(
-              padding: EdgeInsets.only(
-                left: kDefaultPadding,
-                right: kDefaultPadding,
-                bottom: kDefaultPadding + 36,
-              ),
-              height: size.height * 0.2 - 27,
-              decoration: BoxDecoration(
-                color: kPrimaryColor,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(36),
-                  bottomRight: Radius.circular(36)
-                )
-              ),
-              child: Row(
-                children: [
-                  Text(
-                    'SafeWork',
-                    style: Theme.of(context).textTheme.headline5.copyWith(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold
-                    ),
-                  ),
-                  Spacer(),
-                  Image.asset("assets/images/logo.png")
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                height: 54,
-                margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 20),
-                      blurRadius: 50,
-                      color: kPrimaryColor.withOpacity(0.23)
-                    )
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          hintText: "Search",
-                          hintStyle: TextStyle(
-                            color: kPrimaryColor.withOpacity(0.5),
-                          ),
-                          enabledBorder: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                        ),
-                        onChanged: (value) {},
-                      ),
-                    ),
-                    SvgPicture.asset("assets/icons/search.svg"),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      );
   }
 }
